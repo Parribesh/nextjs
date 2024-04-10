c12d0e5d95e7463eeaef87b4650585df5c2e0999
parribesh <neuppar@gmail.com>
2024-04-10 08:29:13 -0500

    some dashboard changes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <p>Customer Page</p>;
+import { fetchCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+
+export default async function Page() {
+  const customer = await fetchCustomers();
+  console.log('customer:', customer);
+  return (
+    <>
+      {customer.map((customer) => (
+        <div>
+          <div>id: {customer.id}</div>
+          <div>name: {customer.name}</div>
+        </div>
+      ))}
+      {/* <CustomersTable customers={customer} /> */}
+    </>
+  );
 }
